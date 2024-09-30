6f84e2e668f1c0c2b4a6fb4a4546a0ca7f52f0ea
MuhammadFaizan402 <faizykhan246@gmail.com>
2024-09-30 11:03:45 +0000

    container config done

---
+++ .devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,14 @@
+"use Strict"; // Treat all Js code are Newer Version
+//alert (3+3) // we are using Node Not a browser
+
+
+let name="Faizan"
+let age = 24
+let isloggedIn= false
+
+// lets check datatype 
+//console.log(typeof name); // result true
+//console.log(typeof age);  // result true
+console.log(typeof isloggedIn); // result true
+//.....................................................................
+
---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=12344321
+let accountEmail="faizykhan246@gmail.com"
+var accountPassword="danger890"
+accountcity="charsadda"
+
+accountcity="peshawar"
+accountPassword="danger098"
+//accountId=2  \\ This is constant and constant can not b change 
+console.table([accountId,accountEmail,accountPassword,accountcity])
